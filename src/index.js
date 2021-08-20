f87405242b26bbcc4c44872ac80cae4a2d5663ef
Hillary Chan <hillary.chan91@gmail.com>
2021-08-20 22:17:51 +1000

    Include config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import firebase from "firebase/app";
-import firebaseConfig from "./firebase-config";
+
+const firebaseConfig = {
+  "apiKey": "AIzaSyBd0Gqvdm-tz3u34CNta6H6br11WTLpIHQ",
+  "appId": "1:997630490578:web:88e0a8336f88c6959e3a0b",
+  "authDomain": "by-the-way-c3675.firebaseapp.com",
+  "messagingSenderId": "997630490578",
+  "projectId": "by-the-way-c3675",
+  "storageBucket": "by-the-way-c3675.appspot.com"
+};
 
 firebase.initializeApp(firebaseConfig);
 

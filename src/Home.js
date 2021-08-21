1c8c3a07ac44ec8018d8b9fa83c853d474e936c1
Hillary Chan <hillary.chan91@gmail.com>
2021-08-21 12:32:36 +1000

    Some UI changes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
+import Container from "react-bootstrap/Container";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container className="mt-2">
+      <h1>My Notes</h1>
+    </Container>
+  );
 }
 
 export default Home;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,15 @@
-import {Container, Nav, NavDropdown, Navbar} from "react-bootstrap";
+import {Button, Container, Nav, NavDropdown, Navbar} from "react-bootstrap";
 import React from "react";
 
 function Navigation() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">btw</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <b>
+            <i>btw</i>
+          </b>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -17,6 +21,9 @@ function Navigation() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Button className="justify-content-end" variant="primary">
+            Sign in with Google
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>

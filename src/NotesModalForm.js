0c5a9f9078a419939e043220174441afb5ad7d4c
Hillary Chan <hillary.chan91@gmail.com>
2021-08-24 17:19:52 +1000

    Form validation

--- src/NotesModalForm.js
+++ src/NotesModalForm.js
@@ -1,20 +1,32 @@
 import React, {Component} from "react";
+import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import PropTypes from "prop-types";
 
 class NotesModalForm extends Component {
-  defaultValues = {
+  defaultState = {
     "description": "",
+    "errors": [],
     "title": ""
   };
 
   constructor(props) {
     super(props);
-    this.state = this.defaultValues;
+    this.state = this.defaultState;
+    this.exitForm = this.exitForm.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.submitForm = this.submitForm.bind(this);
+  }
+
+  resetState() {
+    this.setState(this.defaultState);
+  }
+
+  exitForm() {
+    this.props.handleClose();
+    this.resetState();
   }
 
   handleInputChange(event) {
@@ -22,20 +34,41 @@ class NotesModalForm extends Component {
     this.setState({[target.name]: target.value});
   }
 
+  validateForm() {
+    const {title} = this.state;
+    const {description} = this.state;
+    const errors = [];
+
+    if (title === "" && description === "") {
+      errors.push("cannot create an empty note");
+    } else if (title === "") {
+      errors.push("note title cannot be empty");
+    }
+
+    return errors;
+  }
+
   submitForm(event) {
     event.preventDefault();
-    this.props.handleClose();
-    // Reset form values
-    this.setState(this.defaultValues);
+    const errors = this.validateForm();
+    if (errors.length === 0) {
+      // TODO: store in firestore
+      this.exitForm();
+    }
+    this.setState({errors});
   }
 
   render() {
+    const {errors} = this.state;
     return (
-      <Modal show={this.props.show} onHide={this.props.handleClose}>
+      <Modal show={this.props.show} onHide={this.exitForm}>
         <Modal.Header>
           <Modal.Title>New Note</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          {errors.map((message, idx) => <Alert key={idx} variant="danger">
+              This is a {message} alert—check it out!
+          </Alert>)}
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Title</Form.Label>
@@ -58,7 +91,7 @@ class NotesModalForm extends Component {
                 onChange={this.handleInputChange}
               />
             </Form.Group>
-            <Button variant="secondary" onClick={this.props.handleClose}>
+            <Button variant="secondary" onClick={this.exitForm}>
               Cancel
             </Button>{" "}
             <Button variant="primary" type="submit" onClick={this.submitForm}>

cb73da573a7d713fd6e61d838deca8b0f5f79fc5
Hillary Chan <hillary.chan91@gmail.com>
2021-08-26 15:48:21 +1000

    Update snapshots

--- src/components/__tests__/Alerts.tests.js
+++ src/components/__tests__/Alerts.tests.js
@@ -15,11 +15,43 @@ it("Alerts does not render when no errors", () => {
 it("Alerts renders error message", () => {
   const errors = ["error msg 1"];
   const tree = renderer.create(<Alerts errors={errors} />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+<div
+  className="fade alert alert-danger show"
+  role="alert"
+>
+  Error: 
+  error msg 1
+</div>
+`);
 });
 
 it("Alerts render multiple error message", () => {
-  const errors = ["erro msg 1", "erro msg 2", "erro msg 3"];
+  const errors = ["error msg 1", "error msg 2", "error msg 3"];
   const tree = renderer.create(<Alerts errors={errors} />).toJSON();
-  expect(tree).toMatchInlineSnapshot();
+  expect(tree).toMatchInlineSnapshot(`
+Array [
+  <div
+    className="fade alert alert-danger show"
+    role="alert"
+  >
+    Error: 
+    error msg 1
+  </div>,
+  <div
+    className="fade alert alert-danger show"
+    role="alert"
+  >
+    Error: 
+    error msg 2
+  </div>,
+  <div
+    className="fade alert alert-danger show"
+    role="alert"
+  >
+    Error: 
+    error msg 3
+  </div>,
+]
+`);
 });

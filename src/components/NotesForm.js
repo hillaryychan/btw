4010b9ccf20ecddcca1dd4f7dda228d320c8e10d
Hillary Chan <hillary.chan91@gmail.com>
2021-08-26 16:47:25 +1000

    Add audience via form

--- src/components/NotesForm.js
+++ src/components/NotesForm.js
@@ -1,4 +1,4 @@
-import {Button, Form} from "react-bootstrap";
+import {Badge, Button, Col, Form, Row} from "react-bootstrap";
 import React, {Component} from "react";
 import Alerts from "./Alerts";
 import CharCounter from "./CharCounter";
@@ -9,6 +9,8 @@ const MAX_TITLE_LEN = 100;
 
 class NotesForm extends Component {
   defaultState = {
+    "audience": [],
+    "audienceInput": "",
     "description": "",
     "errors": [],
     "title": ""
@@ -18,8 +20,9 @@ class NotesForm extends Component {
     super(props);
     this.state = this.defaultState;
     this.exitForm = this.exitForm.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
     this.submitForm = this.submitForm.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.addAudience = this.addAudience.bind(this);
   }
 
   resetState() {
@@ -34,6 +37,14 @@ class NotesForm extends Component {
   handleInputChange(event) {
     const {target} = event;
     this.setState({[target.name]: target.value});
+  }
+
+  addAudience() {
+    const {audienceInput} = this.state;
+    this.setState((prevState) => ({
+      "audience": [...prevState.audience, audienceInput],
+      "audienceInput": this.defaultState.audienceInput // reset audienceInput
+    }));
   }
 
   validateForm() {
@@ -98,6 +109,30 @@ class NotesForm extends Component {
             value={this.state.description}
             onChange={this.handleInputChange}
           />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Audience</Form.Label>
+          <Row className="g-2" xs={2}>
+            <Col md>
+              <Form.Control
+                name="audienceInput"
+                as="input"
+                placeholder="Add an audience"
+                value={this.state.audienceInput}
+                onChange={this.handleInputChange}
+              />
+            </Col>
+            <Col md>
+              <Button variant="outline-primary" onClick={this.addAudience}>
+                Add
+              </Button>
+            </Col>
+          </Row>
+          <div>
+            {this.state.audience.map((person, idx) => <Badge pill key={idx} className="m-1">
+              {person}
+            </Badge>)}
+          </div>
         </Form.Group>
         <Button
           variant="secondary"

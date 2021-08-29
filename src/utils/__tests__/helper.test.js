1f7cb238e57173da0ede3e936e2d3d7f71ac4170
Hillary Chan <hillary.chan91@gmail.com>
2021-08-29 12:07:02 +1000

    Test helper functions

--- src/utils/__tests__/helper.test.js
+++ src/utils/__tests__/helper.test.js
@@ -1,25 +1,66 @@
-import {containsDuplicates, normaliseAudience} from "../helper";
+import {
+  canShow,
+  containsDuplicates,
+  createDoc,
+  normaliseAudience
+} from "../helper";
 
-describe("helper function tests", () => {
-  it("containsDuplicates when there are no duplicates", () => {
+describe("canShow", () => {
+  const note = {audience: ["bob", "linda"]};
+  it("assigns true show when falsy filter", () => {
+    expect(canShow(note, "")).toBe(true);
+    expect(canShow(note)).toBe(true);
+    expect(canShow(note, false)).toBe(true);
+  });
+
+  it("assigns true show when given filter exists in audience", () => {
+    expect(canShow(note, "bob")).toBe(true);
+    expect(canShow(note, "linda")).toBe(true);
+  });
+
+  it("assigns false show when given filter does not exists in audience", () => {
+    expect(canShow(note, "louise")).toBe(false);
+  });
+});
+
+describe("containsDuplicates", () => {
+  it("when there are no duplicates", () => {
     const arr = [1, 2, 3];
     expect(containsDuplicates(arr)).toBe(false);
   });
 
-  it("containsDuplicates when there are duplicates", () => {
+  it("when there are duplicates", () => {
     const arr = [1, 2, 2, 3];
     expect(containsDuplicates(arr)).toBe(true);
   });
+});
 
-  it("normaliseAudience converts to lowercase", () => {
+describe("createDoc", () => {
+  it("creates doc object", () => {
+    const ref = "refValue";
+    const note = {
+      audience: ["everyone"],
+      description: "description",
+      lastModified: "now",
+      title: "title"
+    };
+    const doc = createDoc(ref, note, "");
+    expect(doc.ref).toBe(ref);
+    expect(doc.data).toEqual(note);
+    expect(doc.show).toBe(true);
+  });
+});
+
+describe("normaliseAudience", () => {
+  it("converts to lowercase", () => {
     expect(normaliseAudience("LOWERCASE")).toBe("lowercase");
   });
 
-  it("normaliseAudience changes non alphanumeric chars to dash", () => {
+  it("changes non alphanumeric chars to dash", () => {
     expect(normaliseAudience("!@#$%^&*()=+[]{}\\|;:'\"<>,./?")).toBe("-");
   });
 
-  it("normaliseAudience converts input corrects", () => {
+  it("converts input corrects", () => {
     expect(normaliseAudience("  hOp #$%poP     % ")).toBe("-hop-pop-");
   });
 });
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -12,21 +12,10 @@ function createDoc(refCode, note, filter) {
   return {data: note, ref: refCode, show: canShow(note, filter)};
 }
 
-function incrementCounter(counter, key) {
-  counter[key] = (counter[key] || 0) + 1;
-  return counter;
-}
-
 function normaliseAudience(member) {
   let normalised = member.toLowerCase();
   normalised = normalised.replace(/\W+/gu, "-");
   return normalised;
 }
 
-export {
-  canShow,
-  containsDuplicates,
-  createDoc,
-  incrementCounter,
-  normaliseAudience
-};
+export {canShow, containsDuplicates, createDoc, normaliseAudience};
